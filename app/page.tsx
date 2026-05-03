031805478c38a330838bc2daa8913d5d140ff92a
jerome <jeromechong229@gmail.com>
2026-05-03 14:14:53 +0100

    scaffold supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
 import Image from "next/image";
+import { createClient } from '../utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Home() {
   return (
